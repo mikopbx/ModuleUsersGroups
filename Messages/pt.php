affe90bf525994f9c9faf82d1ca90b9ed8264c1a
Everton Massen Goncalves <massen@gmail.com>
2021-10-16 03:19:00 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 3.8% (1 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/pt/

--- Messages/pt.php
+++ Messages/pt.php
@@ -1,1 +1,11 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Módulo de Grupo de Usuários',
+];
