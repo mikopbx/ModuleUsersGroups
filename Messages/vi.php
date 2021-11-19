e4e142b8828e81cbd4530bff00a3bad178eaf700
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:21:52 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Mô-đun nhóm người dùng -% repesent%',
+    'mo_ModuleModuleUsersGroups' => 'Mô-đun nhóm người dùng',
+    'BreadcrumbModuleUsersGroups' => 'Nhóm người dùng',
+    'SubHeaderModuleUsersGroups' => 'Đặt quyền cho các tuyến đường đi',
+    'mod_usrgr_TextFieldLabel' => 'Ví dụ về hộp văn bản',
+    'mod_usrgr_TextAreaFieldLabel' => 'Ví dụ về trường nhiều trang',
+    'mod_usrgr_PasswordFieldLabel' => 'Ví dụ về trường mật khẩu',
+    'mod_usrgr_IntegerFieldLabel' => 'Ví dụ về trường số',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Hộp kiểm',
+    'mod_usrgr_ToggleFieldLabel' => 'Chuyển',
+    'mod_usrgr_DropDownFieldLabel' => 'Trình đơn thả xuống',
+    'mod_usrgr_Connected' => 'Mô-đun được kết nối',
+    'mod_usrgr_Disconnected' => 'Mô-đun bị vô hiệu hóa',
+    'mod_usrgr_Groups' => 'Nhóm nhân viên',
+    'mod_usrgr_Users' => 'Nhân viên',
+    'mod_usrgr_AddNewUsersGroup' => 'Tạo một nhóm nhân viên',
+    'mod_usrgr_ColumnGroupName' => 'Tập đoàn',
+    'mod_usrgr_ColumnGroupDescription' => 'Mô tả',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Số lượng người tham gia',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Kiểm tra trường với tên của nhóm',
+    'mod_usrgr_ColumnGroup' => 'Tập đoàn',
+    'mod_usrgr__GeneralSettings' => 'Cài đặt nhóm',
+    'mod_usrgr_UsersFilter' => 'Nhân viên nhóm',
+    'mod_usrgr_RoutingRules' => 'Quy tắc định tuyến đi',
+    'mod_usrgr_ColumnCallerId' => 'Người gọi ID',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Chọn một nhân viên',
+];
