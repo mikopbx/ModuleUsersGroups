211fde8c4fa53142699764a1cb4139110761b8a7
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:28:01 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'მომხმარებელთა ჯგუფის მოდული -% repesent%',
+    'mo_ModuleModuleUsersGroups' => 'მომხმარებელთა ჯგუფის მოდული',
+    'BreadcrumbModuleUsersGroups' => 'მომხმარებელთა ჯგუფები',
+    'SubHeaderModuleUsersGroups' => 'გამავალი მარშრუტების უფლებების დაყენება',
+    'mod_usrgr_TextFieldLabel' => 'ტექსტური ყუთის მაგალითი',
+    'mod_usrgr_TextAreaFieldLabel' => 'მრავალგვერდიანი ველის მაგალითი',
+    'mod_usrgr_PasswordFieldLabel' => 'პაროლის ველის მაგალითი',
+    'mod_usrgr_IntegerFieldLabel' => 'რიცხვითი ველის მაგალითი',
+    'mod_usrgr_CheckBoxFieldLabel' => 'მოსანიშნი ველი',
+    'mod_usrgr_ToggleFieldLabel' => 'გადართვა',
+    'mod_usrgr_DropDownFieldLabel' => 'ჩამოსაშლელი მენიუ',
+    'mod_usrgr_Connected' => 'მოდული დაკავშირებულია',
+    'mod_usrgr_Disconnected' => 'მოდული გამორთულია',
+    'mod_usrgr_Groups' => 'თანამშრომელთა ჯგუფები',
+    'mod_usrgr_Users' => 'პერსონალი',
+    'mod_usrgr_AddNewUsersGroup' => 'შექმენით თანამშრომლების ჯგუფი',
+    'mod_usrgr_ColumnGroupName' => 'ჯგუფი',
+    'mod_usrgr_ColumnGroupDescription' => 'აღწერა',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Მონაწილეთა რაოდენობა',
+    'mod_usrgr_ValidateNameIsEmpty' => 'შეამოწმეთ ველი ჯგუფის სახელით',
+    'mod_usrgr_ColumnGroup' => 'ჯგუფი',
+    'mod_usrgr__GeneralSettings' => 'ჯგუფის პარამეტრები',
+    'mod_usrgr_UsersFilter' => 'ჯგუფის პერსონალი',
+    'mod_usrgr_RoutingRules' => 'გამავალი მარშრუტის წესები',
+    'mod_usrgr_ColumnCallerId' => 'Აბონენტის საიდენტიფიკაციო ნომერი',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'აირჩიეთ თანამშრომელი',
+];
