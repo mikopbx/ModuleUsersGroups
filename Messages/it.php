3d7b9b03126f8de22a869799f39edc1bd4107e83
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:16:10 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/it/

--- Messages/it.php
+++ Messages/it.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Modulo gruppo utenti -% repesent%',
+    'mo_ModuleModuleUsersGroups' => 'Modulo gruppo utenti',
+    'BreadcrumbModuleUsersGroups' => 'Gruppi di utenti',
+    'SubHeaderModuleUsersGroups' => 'Impostazione dei diritti per le rotte in uscita',
+    'mod_usrgr_TextFieldLabel' => 'Esempio di casella di testo',
+    'mod_usrgr_TextAreaFieldLabel' => 'Esempio di un campo multipagina',
+    'mod_usrgr_PasswordFieldLabel' => 'Esempio di campo password',
+    'mod_usrgr_IntegerFieldLabel' => 'Esempio di campo numerico',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Casella di controllo',
+    'mod_usrgr_ToggleFieldLabel' => 'Interruttore',
+    'mod_usrgr_DropDownFieldLabel' => 'Menu a discesa',
+    'mod_usrgr_Connected' => 'Modulo connesso',
+    'mod_usrgr_Disconnected' => 'Modulo disabilitato',
+    'mod_usrgr_Groups' => 'Gruppi di dipendenti',
+    'mod_usrgr_Users' => 'personale',
+    'mod_usrgr_AddNewUsersGroup' => 'Crea un gruppo di dipendenti',
+    'mod_usrgr_ColumnGroupName' => 'Gruppo',
+    'mod_usrgr_ColumnGroupDescription' => 'descrizione',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Numero di partecipanti',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Spunta il campo con il nome del gruppo',
+    'mod_usrgr_ColumnGroup' => 'Gruppo',
+    'mod_usrgr__GeneralSettings' => 'Impostazioni di gruppo',
+    'mod_usrgr_UsersFilter' => 'Personale del gruppo',
+    'mod_usrgr_RoutingRules' => 'Regole di instradamento in uscita',
+    'mod_usrgr_ColumnCallerId' => 'ID chiamante',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Seleziona un dipendente',
+];
