6ba6078013be88448d053e88ebf04b5271f6cff3
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:25:17 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/ja/

--- Messages/ja.php
+++ Messages/ja.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'ユーザーグループモジュール-％repesent％',
+    'mo_ModuleModuleUsersGroups' => 'ユーザーグループモジュール',
+    'BreadcrumbModuleUsersGroups' => 'ユーザーのグループ',
+    'SubHeaderModuleUsersGroups' => '発信ルートの設定権',
+    'mod_usrgr_TextFieldLabel' => 'テキストボックスの例',
+    'mod_usrgr_TextAreaFieldLabel' => 'マルチページフィールドの例',
+    'mod_usrgr_PasswordFieldLabel' => 'パスワードフィールドの例',
+    'mod_usrgr_IntegerFieldLabel' => '数値フィールドの例',
+    'mod_usrgr_CheckBoxFieldLabel' => 'チェックボックス',
+    'mod_usrgr_ToggleFieldLabel' => 'スイッチ',
+    'mod_usrgr_DropDownFieldLabel' => 'ドロップダウンメニュー',
+    'mod_usrgr_Connected' => 'モジュールが接続されています',
+    'mod_usrgr_Disconnected' => 'モジュールが無効になっています',
+    'mod_usrgr_Groups' => '従業員グループ',
+    'mod_usrgr_Users' => '従業員',
+    'mod_usrgr_AddNewUsersGroup' => '従業員のグループを作成する',
+    'mod_usrgr_ColumnGroupName' => 'グループ',
+    'mod_usrgr_ColumnGroupDescription' => '説明文',
+    'mod_usrgr_ColumnGroupMembersCount' => '参加者の数',
+    'mod_usrgr_ValidateNameIsEmpty' => 'グループの名前でフィールドを確認します',
+    'mod_usrgr_ColumnGroup' => 'グループ',
+    'mod_usrgr__GeneralSettings' => 'グループ設定',
+    'mod_usrgr_UsersFilter' => 'グループスタッフ',
+    'mod_usrgr_RoutingRules' => 'アウトバウンドルーティングルール',
+    'mod_usrgr_ColumnCallerId' => '発信者ID',
+    'mod_usrgr_SelectMemberToAddToGroup' => '従業員を選択する',
+];
