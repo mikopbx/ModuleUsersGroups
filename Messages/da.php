58cba317dad30fc1df86854f7ea9781ff5e823e4
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:13:42 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/da/

--- Messages/da.php
+++ Messages/da.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Brugergruppemodul - % repræsenterer %',
+    'mo_ModuleModuleUsersGroups' => 'Brugergruppemodul',
+    'BreadcrumbModuleUsersGroups' => 'Grupper af brugere',
+    'SubHeaderModuleUsersGroups' => 'Indstilling af rettigheder for udgående ruter',
+    'mod_usrgr_TextFieldLabel' => 'Eksempel på en tekstboks',
+    'mod_usrgr_TextAreaFieldLabel' => 'Eksempel på et flersidet felt',
+    'mod_usrgr_PasswordFieldLabel' => 'Eksempel på adgangskodefelt',
+    'mod_usrgr_IntegerFieldLabel' => 'Eksempel på numerisk felt',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Afkrydsningsfelt',
+    'mod_usrgr_ToggleFieldLabel' => 'Kontakt',
+    'mod_usrgr_DropDownFieldLabel' => 'Drop down menu',
+    'mod_usrgr_Connected' => 'Modul tilsluttet',
+    'mod_usrgr_Disconnected' => 'Modul deaktiveret',
+    'mod_usrgr_Groups' => 'Medarbejdergrupper',
+    'mod_usrgr_Users' => 'personale',
+    'mod_usrgr_AddNewUsersGroup' => 'Opret en gruppe medarbejdere',
+    'mod_usrgr_ColumnGroupName' => 'Gruppe',
+    'mod_usrgr_ColumnGroupDescription' => 'beskrivelse',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Antal deltagere',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Marker feltet med navnet på gruppen',
+    'mod_usrgr_ColumnGroup' => 'Gruppe',
+    'mod_usrgr__GeneralSettings' => 'Gruppeindstillinger',
+    'mod_usrgr_UsersFilter' => 'Gruppens personale',
+    'mod_usrgr_RoutingRules' => 'Regler for udgående routing',
+    'mod_usrgr_ColumnCallerId' => 'Vis nummer',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Vælg en medarbejder',
+];
