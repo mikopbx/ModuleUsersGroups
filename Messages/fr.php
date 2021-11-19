1bffb3d184584eb8a720e449e1f36a5a6fea3ab8
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:22:58 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/fr/

--- Messages/fr.php
+++ Messages/fr.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Module de groupe d\'utilisateurs -% représentant%',
+    'mo_ModuleModuleUsersGroups' => 'Module de groupe d\'utilisateurs',
+    'BreadcrumbModuleUsersGroups' => 'Groupes d\'utilisateurs',
+    'SubHeaderModuleUsersGroups' => 'Paramétrage des droits pour les itinéraires sortants',
+    'mod_usrgr_TextFieldLabel' => 'Exemple de zone de texte',
+    'mod_usrgr_TextAreaFieldLabel' => 'Exemple de champ multipage',
+    'mod_usrgr_PasswordFieldLabel' => 'Exemple de champ de mot de passe',
+    'mod_usrgr_IntegerFieldLabel' => 'Exemple de champ numérique',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Case à cocher',
+    'mod_usrgr_ToggleFieldLabel' => 'Changer',
+    'mod_usrgr_DropDownFieldLabel' => 'Menu déroulant',
+    'mod_usrgr_Connected' => 'Module connecté',
+    'mod_usrgr_Disconnected' => 'Module désactivé',
+    'mod_usrgr_Groups' => 'Groupes d\'employés',
+    'mod_usrgr_Users' => 'Les employés',
+    'mod_usrgr_AddNewUsersGroup' => 'Créer un groupe d\'employés',
+    'mod_usrgr_ColumnGroupName' => 'Grouper',
+    'mod_usrgr_ColumnGroupDescription' => 'La description',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Nombre de participants',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Vérifiez le champ avec le nom du groupe',
+    'mod_usrgr_ColumnGroup' => 'Grouper',
+    'mod_usrgr__GeneralSettings' => 'Paramètres de groupe',
+    'mod_usrgr_UsersFilter' => 'Personnel du groupe',
+    'mod_usrgr_RoutingRules' => 'Règles de routage sortant',
+    'mod_usrgr_ColumnCallerId' => 'Identification de l\'appelant',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Sélectionnez un employé',
+];
