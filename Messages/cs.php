94740cf0388af9c820e0ef25810222354d486383
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:18:21 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Modul uživatelské skupiny - % reprezentativní%',
+    'mo_ModuleModuleUsersGroups' => 'Modul skupiny uživatelů',
+    'BreadcrumbModuleUsersGroups' => 'Skupiny uživatelů',
+    'SubHeaderModuleUsersGroups' => 'Nastavení práv pro odchozí cesty',
+    'mod_usrgr_TextFieldLabel' => 'Příklad textového pole',
+    'mod_usrgr_TextAreaFieldLabel' => 'Příklad vícestránkového pole',
+    'mod_usrgr_PasswordFieldLabel' => 'Příklad pole hesla',
+    'mod_usrgr_IntegerFieldLabel' => 'Příklad číselného pole',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Zaškrtávací políčko',
+    'mod_usrgr_ToggleFieldLabel' => 'Přepínač',
+    'mod_usrgr_DropDownFieldLabel' => 'Rozbalovací nabídka',
+    'mod_usrgr_Connected' => 'Modul připojen',
+    'mod_usrgr_Disconnected' => 'Modul je deaktivován',
+    'mod_usrgr_Groups' => 'Skupiny zaměstnanců',
+    'mod_usrgr_Users' => 'Zaměstnanci',
+    'mod_usrgr_AddNewUsersGroup' => 'Vytvořte skupinu zaměstnanců',
+    'mod_usrgr_ColumnGroupName' => 'Skupina',
+    'mod_usrgr_ColumnGroupDescription' => 'Popis',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Počet zúčastněných',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Zaškrtněte pole s názvem skupiny',
+    'mod_usrgr_ColumnGroup' => 'Skupina',
+    'mod_usrgr__GeneralSettings' => 'Nastavení skupiny',
+    'mod_usrgr_UsersFilter' => 'Zaměstnanci skupiny',
+    'mod_usrgr_RoutingRules' => 'Pravidla odchozího směrování',
+    'mod_usrgr_ColumnCallerId' => 'volajícího ID',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Vyberte zaměstnance',
+];
