e6e41821e61061e695b35d9a9d8e40f10cffc81a
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:23:35 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/uk/

--- Messages/uk.php
+++ Messages/uk.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Модуль групи користувачів - %repesent%',
+    'mo_ModuleModuleUsersGroups' => 'Модуль групи користувачів',
+    'BreadcrumbModuleUsersGroups' => 'Групи користувачів',
+    'SubHeaderModuleUsersGroups' => 'Налаштування прав для вихідних маршрутів',
+    'mod_usrgr_TextFieldLabel' => 'Приклад текстового поля',
+    'mod_usrgr_TextAreaFieldLabel' => 'Приклад багатосторінкового поля',
+    'mod_usrgr_PasswordFieldLabel' => 'Приклад поля з паролем',
+    'mod_usrgr_IntegerFieldLabel' => 'Приклад числового поля',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Чекбокс',
+    'mod_usrgr_ToggleFieldLabel' => 'Перемикач',
+    'mod_usrgr_DropDownFieldLabel' => 'Випадаюче меню',
+    'mod_usrgr_Connected' => 'Модуль підключено',
+    'mod_usrgr_Disconnected' => 'Модуль відключений',
+    'mod_usrgr_Groups' => 'Групи співробітників',
+    'mod_usrgr_Users' => 'співробітники',
+    'mod_usrgr_AddNewUsersGroup' => 'Створити групу співробітників',
+    'mod_usrgr_ColumnGroupName' => 'Група',
+    'mod_usrgr_ColumnGroupDescription' => 'Опис',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Кількість учасників',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Перевірте поле під назвою групи',
+    'mod_usrgr_ColumnGroup' => 'Група',
+    'mod_usrgr__GeneralSettings' => 'Налаштування групи',
+    'mod_usrgr_UsersFilter' => 'Співробітники групи',
+    'mod_usrgr_RoutingRules' => 'Правила вихідної маршрутизації',
+    'mod_usrgr_ColumnCallerId' => 'Caller ID',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Виберіть співробітника',
+];
