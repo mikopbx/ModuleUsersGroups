01246e7e8b977ebeddd0a219ee80b233a24845d0
Voutsas Theocharis <voutsasth@yahoo.gr>
2022-06-16 19:19:36 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/el/

--- Messages/el.php
+++ Messages/el.php
@@ -1,1 +1,36 @@
 <?php
+return [
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Επιλέξτε έναν χρήστη',
+    'mod_usrgr_ColumnCallerId' => 'Αναγνωριστικό καλούντος (Caller ID)',
+    'mod_usrgr_RoutingRules' => 'Κανόνες εξερχόμενης δρομολόγησης',
+    'mod_usrgr_UsersFilter' => 'Υπάλληλοι του Ομίλου',
+    'mod_usrgr__GeneralSettings' => 'Ρυθμίσεις ομάδας',
+    'mod_usrgr_ColumnGroup' => 'Ομάδα',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Ελέγξτε το πεδίο ονόματος ομάδας',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Αριθμός συμμετεχόντων',
+    'mod_usrgr_ColumnGroupDescription' => 'Περιγραφή',
+    'mod_usrgr_ColumnGroupName' => 'Ομάδα',
+    'mod_usrgr_AddNewUsersGroup' => 'Δημιουργήστε μια ομάδα χρηστών',
+    'mod_usrgr_Users' => 'Εκχώρηση ρόλων στους χρήστες',
+    'mod_usrgr_Groups' => 'Ομάδες χρηστών',
+    'mod_usrgr_Disconnected' => 'Η μονάδα απενεργοποιήθηκε',
+    'mod_usrgr_Connected' => 'Η μονάδα είναι συνδεδεμένη',
+    'mod_usrgr_DropDownFieldLabel' => 'Πτυσώμενο μενού',
+    'mod_usrgr_ToggleFieldLabel' => 'Διακόπτης',
+    'mod_usrgr_CheckBoxFieldLabel' => 'Πλαίσιο ελέγχου',
+    'mod_usrgr_IntegerFieldLabel' => 'Παράδειγμα αριθμητικού πεδίου',
+    'mod_usrgr_PasswordFieldLabel' => 'Παράδειγμα πεδίου κωδικού πρόσβασης',
+    'mod_usrgr_TextAreaFieldLabel' => 'Παράδειγμα πολυσέλιδου πεδίου',
+    'mod_usrgr_TextFieldLabel' => 'Παράδειγμα πεδίου κειμένου',
+    'SubHeaderModuleUsersGroups' => 'Ρύθμιση δικαιωμάτων για εξερχόμενες κλήσεις',
+    'BreadcrumbModuleUsersGroups' => 'Ομάδα χρηστών',
+    'mo_ModuleModuleUsersGroups' => 'Μονάδα ομάδας χρηστών',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleUsersGroups' => 'Μονάδα ομάδας χρηστών - %repesent%',
+];
