e2921a96aa97ffb8b19cce528a6557c6c689f7bd
Anonymous <noreply@weblate.org>
2023-09-01 12:01:21 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/ro/

--- Messages/ro.php
+++ Messages/ro.php
@@ -1,1 +1,57 @@
 <?php
+return [
+    'mod_usrgr_PatternsInstructions7' => '7XXXXXXXXX - orice număr de 11 cifre care începe cu 7',
+    'BreadcrumbModuleUsersGroups' => 'Gestionarea grupului de apelare',
+    'mo_ModuleModuleUsersGroups' => 'Gestionarea grupului de apelare',
+    'SubHeaderModuleUsersGroups' => 'Setarea drepturilor pentru apelurile efectuate, gestionarea ID apelantului de ieșire, organizarea grupurilor de preluare a apelurilor',
+    'mod_usrgr_Connected' => 'Modul conectat',
+    'mod_usrgr_Disconnected' => 'Modul dezactivat',
+    'mod_usrgr_isolate' => 'Izolați un grup de angajați',
+    'mod_usrgr_isolatePickUp' => 'Izolați funcția de preluare',
+    'mod_usrgr_Users' => 'Angajatii',
+    'mod_usrgr_patterns' => 'Modele de numere legate de grup. Un membru al grupului îi va putea suna doar',
+    'mod_usrgr_Groups' => 'Apelați lista grupurilor',
+    'mod_usrgr_ColumnCallerId' => 'ID-ul apelantului',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Alegeți un angajat',
+    'mod_usrgr_SelectUserGroup' => 'Selectați un plan de apelare pentru angajat',
+    'BreadcrumbModuleUsersGroupsModify' => 'Configurarea unui plan de apelare',
+    'mod_usrgr_AddNewUsersGroup' => 'Creați un plan de apelare',
+    'mod_usrgr_ColumnGroupName' => 'grup',
+    'mod_usrgr_ColumnGroupDescription' => 'Descriere',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Numărul de participanți',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Verificați câmpul pentru numele grupului',
+    'mod_usrgr_ColumnGroup' => 'grup',
+    'mod_usrgr_PatternsInstructions2' => 'Un membru al grupului va putea să formeze numai numere care se potrivesc cu modelele',
+    'mod_usrgr_PatternsInstructions4' => 'Exemple de șabloane:',
+    'mod_usrgr_PatternsInstructions5' => '2XX - numere de la 200 la 299',
+    'mod_usrgr_PatternsInstructions6' => '200001 - un număr intern specificat explicit, de exemplu, un număr de coadă',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'repModuleUsersGroups' => 'Modul de apelare grupuri - %repesent%',
+    'mod_usrgr__GeneralSettings' => 'Setări de grup',
+    'mod_usrgr_UsersFilter' => 'Angajații grupului',
+    'mod_usrgr_RoutingRules' => 'Reguli de rutare de ieșire',
+    'mod_usrgr_PatternsInstructions1' => 'În modele, este permisă utilizarea unui caracter de la 1 la 9 și a caracterului X (orice număr de la 1 la 9)',
+    'mod_usrgr_IsolateInstructions1' => 'Membrii grupului vor putea apela doar la numerele grupului lor.',
+    'mod_usrgr_IsolateInstructions2' => 'Angajații din alte grupuri nu vor putea apela grupul izolat.',
+    'mod_usrgr_ColumnDefaultGroup' => 'grup implicit',
+    'mod_usrgr_ErrorOnDeleteDefaultGroup' => 'Nu se poate șterge grupul prestabilit',
+    'mod_usrgr_SelectDefaultGroup' => 'Alegeți un grup',
+    'mod_usrgr_DefaultGroup' => 'grup implicit',
+    'mod_usrgr_PatternsInstructions3' => ' ',
+];
