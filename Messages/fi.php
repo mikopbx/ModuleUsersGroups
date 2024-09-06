f5f56dce4b9b2a293abe6384943c8bde327992bb
Anonymous <noreply@weblate.org>
2024-09-06 04:01:07 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleUsersGroups
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleusersgroups/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -1,1 +1,57 @@
 <?php
+return [
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'repModuleUsersGroups' => 'Puhelinryhmämoduuli - %repesent%',
+    'mo_ModuleModuleUsersGroups' => 'Dial Group Management',
+    'BreadcrumbModuleUsersGroups' => 'Dial Group Management',
+    'BreadcrumbModuleUsersGroupsModify' => 'Soittoryhmän määrittäminen',
+    'SubHeaderModuleUsersGroups' => 'Lähtevien puheluiden oikeuksien määrittäminen, lähtevien soittajan tunnuksen hallinta, puheluiden poimintaryhmien järjestäminen',
+    'mod_usrgr_patterns' => 'Ryhmään liittyvien numeroiden mallit. Ryhmän jäsen voi soittaa vain näihin numeroihin',
+    'mod_usrgr_Connected' => 'Moduuli kytketty',
+    'mod_usrgr_Disconnected' => 'Moduuli poistettu käytöstä',
+    'mod_usrgr_isolate' => 'Eristä joukko työntekijöitä',
+    'mod_usrgr_isolatePickUp' => 'Eristä noutotoiminto',
+    'mod_usrgr_Groups' => 'Luettelo valintaryhmistä',
+    'mod_usrgr_Users' => 'Työntekijät',
+    'mod_usrgr_AddNewUsersGroup' => 'Luo soittoryhmä',
+    'mod_usrgr_ColumnGroupName' => 'ryhmä',
+    'mod_usrgr_ColumnGroupDescription' => 'Kuvaus',
+    'mod_usrgr_ColumnGroupMembersCount' => 'Osallistujien määrä',
+    'mod_usrgr_ValidateNameIsEmpty' => 'Tarkista ryhmän nimi -kenttä',
+    'mod_usrgr_ColumnGroup' => 'ryhmä',
+    'mod_usrgr__GeneralSettings' => 'Ryhmäasetukset',
+    'mod_usrgr_UsersFilter' => 'Konsernin työntekijät',
+    'mod_usrgr_RoutingRules' => 'Lähtevän liikenteen reitityssäännöt',
+    'mod_usrgr_ColumnCallerId' => 'Soittajan tunnus',
+    'mod_usrgr_SelectMemberToAddToGroup' => 'Valitse työntekijä',
+    'mod_usrgr_SelectUserGroup' => 'Valitse työntekijälle puhelinryhmä',
+    'mod_usrgr_PatternsInstructions1' => 'Malleissa voidaan käyttää symbolia 1-9 ja symbolia X (mitä tahansa numeroa 1-9)',
+    'mod_usrgr_PatternsInstructions2' => 'Ryhmän jäsen voi valita vain niitä numeroita, jotka vastaavat kuvioita',
+    'mod_usrgr_PatternsInstructions3' => ' ',
+    'mod_usrgr_PatternsInstructions4' => 'Esimerkkejä malleista:',
+    'mod_usrgr_PatternsInstructions5' => '2XX - numerot 200 - 299',
+    'mod_usrgr_PatternsInstructions6' => '200001 - eksplisiittisesti määritetty sisäinen numero, esimerkiksi jononumero',
+    'mod_usrgr_PatternsInstructions7' => '7XXXXXXXXXX - mikä tahansa 11-numeroinen numero, joka alkaa 7:llä',
+    'mod_usrgr_IsolateInstructions1' => 'Ryhmän työntekijät voivat soittaa vain ryhmänsä numeroihin.',
+    'mod_usrgr_IsolateInstructions2' => 'Muiden ryhmien työntekijät eivät voi soittaa eristettyyn ryhmään.',
+    'mod_usrgr_ColumnDefaultGroup' => 'Oletusryhmä',
+    'mod_usrgr_ErrorOnDeleteDefaultGroup' => 'Oletusryhmää ei voi poistaa',
+    'mod_usrgr_SelectDefaultGroup' => 'Valitse ryhmä',
+    'mod_usrgr_DefaultGroup' => 'Oletusryhmä',
+];
